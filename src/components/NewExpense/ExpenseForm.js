0b89b92e4750861bf9bb612d4c8ad8ce1e49124b
Amit Kumar <amitverma1150@gmail.com>
2021-05-08 09:34:57 +0530

    bug fixing

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -46,7 +46,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: userInput.enteredTitle,
-      amount: userInput.enteredAmount,
+      amount: +userInput.enteredAmount,
       date: new Date(userInput.enteredDate)
     }
     props.onSaveExpenseDate(expenseData);
